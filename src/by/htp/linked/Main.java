6b8ad6e1560149ca17a1cda4a97453b27edffe14
Fevor <Fevor@tut.by>
2017-04-18 00:59:36 +0300

    Added various ways to add and receive elmenites.

--- src/by/htp/linked/Main.java
+++ src/by/htp/linked/Main.java
@@ -1,26 +1,23 @@
 package by.htp.linked;
- 
- public class Main {
- 
- 	public static void main(String[] args) {
- 		// TODO Auto-generated method stub
- 		//String line = new String();
- 		String s="11";
- 		//System.out.println(s);
- 		MyLinkedList<String> list = new MyLinkedList<String>();
- 		list.add(s);
- 		list.add("22");
- 		list.add("33");
- 		list.add("44");
- 		list.add("52");
- 		list.add("66");
- 		System.out.println(list.get4(5));
- 		System.out.println(list.get(1));
- 		for (int i =0; i<list.getSize(); i  ){
- 			System.out.println(list.get4(i));
- 		}
- 		System.out.println(list.getSize());
- 		//System.out.println(list.getFirst());
- 		
- 	}
- }
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyLinkedList<String> list = new MyLinkedList<String>();
+		list.add("11");
+		list.add("22");
+		list.add("33");
+		list.add("44");
+		list.add("52");
+		list.add("66");
+		list.add(3, "777");
+		
+		for (int i = 0; i < list.getSize(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		System.out.println(list.getSize());
+
+	}
+}
--- src/by/htp/linked/MyLinkedList.java
+++ src/by/htp/linked/MyLinkedList.java
@@ -1,99 +1,152 @@
 package by.htp.linked;
- 
- public class MyLinkedList<E> {
- 	private int size = 0;
- 	private Element<E> element;
- 	private Element<E> first;
- 	private Element<E> last;
- 
- 	public MyLinkedList() {
- 		element = new Element<E>();
- 		first = element.prev;
- 		last = element.next;
- 	}
- 
- 	public int getSize() {
- 		return size;
- 	}
- 
- 	public void add(E e) {
- 		if (size == 0) {
- 			Element<E> element = new Element<E>();
- 			first = last = element;
- 			element.next = element.prev = first;
- 			element.e = e;
- 		} else {
- 			Element<E> element = new Element<E>(e, last, first);
- 			Element<E> element2 = last;
- 			element2.next = element;
- 			last = element;
- 		}
- 		element.e = e;
- 		size  ;
- 	}
- 
- 	public E get(int namber) {
- 
- 		Element<E> element = first;
- 		for (int i = 0; i < namber; i  ) {
- 			element = element.next;
- 		}
- 		return element.e;
- 	}
- 
- 	public E get2(int namber) {
- 
- 		Element<E> element = last;
- 		for (int i = size; i > namber; i--) {
- 			element = element.next;
- 		}
- 		return element.e;
- 	}
- 
- 	public E get3(int namber) {
- 
- 		Element<E> element = first;
- 		for (int i = 0; i < namber; i  ) {
- 			element = element.next;
- 		}
- 		return element.e;
- 	}
- 
- 	public E get4(int namber) {
- 		boolean b = ((namber) < (size / 2));
- 		if (b) {
- 			Element<E> element = first;
- 			for (int i = 0; i < namber; i  ) {
- 				element = element.next;
- 			}
- 			return element.e;
- 		} else {
- 			Element<E> element = last;
- 			for (int i = (size-1); i > namber; i--) {
- 				element = element.prev;
- 			}
- 			return element.e;
- 		}
- 
- 		//return element.e;
- 
- 	}
- 
- 	private class Element<E> {
- 		private E e;
- 		private Element<E> prev;
- 		private Element<E> next;
- 
- 		public Element() {
- 			this.e = null;
- 			this.prev = null;
- 			this.next = null;
- 		}
- 
- 		public Element(E e, Element prev, Element next) {
- 			this.e = e;
- 			this.prev = prev;
- 			this.next = next;
- 		}
- 	}
- }
+
+public class MyLinkedList<E> {
+	private int size = 0;
+	private Element<E> element;
+	private Element<E> first;
+	private Element<E> last;
+
+	public MyLinkedList() {
+		element = new Element<E>();
+		first = element.prev;
+		last = element.next;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void add(E e) {
+		if (size == 0) {
+			Element<E> el = new Element<E>();
+			first = last = el;
+			el.next = el.prev = first;
+			el.e = e;
+		} else {
+			Element<E> el = new Element<E>(e, last, first);
+			last.next = el;
+			last = el;
+		}
+		size++;
+	}
+
+	public void add(int namber, E e) {
+		int si=size-1;
+		if (namber == 0) {
+			addFirst(e);
+		}
+		if (namber == si) {
+			add(e);
+		}
+		if ((namber > 0) && (namber < si)) {
+			addTheMiddle(namber, e);
+		}
+		if (namber > si){
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+	}
+
+	private void addTheMiddle(int namber, E e) {
+			Element<E> el = elementSearch2(namber);
+			Element<E> el2 = new Element<E>(e, el.prev, el);
+			Element<E> el3 = el.prev;
+			el.prev = el2;
+			el3.next = el2;
+			size++;
+	}
+
+	public void addLast(E e) {
+		add(e);
+	}
+
+	public void addFirst(E e) {
+		if (size == 0) {
+			Element<E> el = new Element<E>(e, first, first);
+			first = last = el;
+
+		} else {
+			Element<E> el = new Element<E>(e, first.prev, first);
+			Element<E> el2 = first;
+			el2.prev = el;
+			first = el;
+
+		}
+		size++;
+	}
+
+	public E getFirst() {
+		return first.e;
+	}
+
+	public E getLast() {
+		return last.e;
+	}
+
+	public E get(int namber) {
+		E el = null;
+		if ((namber >= 0) && (namber < size)) {
+			el = elementSearch(namber);
+		} else {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		return el;
+	}
+
+	private E elementSearch(int namber) {
+		if (halfValue(namber)) {
+			Element<E> el = first;
+			for (int i = 0; i < namber; i++) {
+				el = el.next;
+			}
+			return el.e;
+		} else {
+			Element<E> el = last;
+			for (int i = (size - 1); i > namber; i--) {
+				el = el.prev;
+			}
+			return el.e;
+		}
+	}
+
+	private Element<E> elementSearch2(int namber) {
+		if (halfValue(namber)) {
+			Element<E> el = first;
+			for (int i = 0; i < namber; i++) {
+				el = el.next;
+			}
+			return el;
+		} else {
+			Element<E> el = last;
+			for (int i = (size - 1); i > namber; i--) {
+				el = el.prev;
+			}
+			return el;
+		}
+	}
+
+	private boolean halfValue(int namber) {
+		return ((namber) < (size / 2));
+	}
+
+	private class Element<E> {
+		private E e;
+		private Element<E> prev;
+		private Element<E> next;
+
+		public Element() {
+			this.e = null;
+			this.prev = null;
+			this.next = null;
+		}
+
+		public Element(E e) {
+			this.e = e;
+		}
+
+		public Element(E e, Element prev, Element next) {
+			this.e = e;
+			this.prev = prev;
+			this.next = next;
+		}
+	}
+}
